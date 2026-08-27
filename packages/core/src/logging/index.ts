53025be4465fb6a6b9def155fd5686d0a2ed4d1a
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-27 09:22:06 +0100

    refactor(core): update logging barrel exports

--- packages/core/src/logging/index.ts
+++ packages/core/src/logging/index.ts
@@ -1,39 +1,9 @@
-export {
-  BaseLogger,
-  type Logger,
-  type LogContext,
-} from "./logger.js";
+/**
+ * @lattice/core/logging
+ *
+ * Logging abstractions and structured log output.
+ */
 
-export {
-  LogLevel,
-  LogLevelPriority,
-  shouldLog,
-  type LogLevel as LogLevelType,
-} from "./log-level.js";
+export * from "./core/index.js";
 
-export {
-  createLoggerContext,
-  type LoggerContext,
-} from "./logger-context.js";
-
-export {
-  serializeLogError,
-  type LogEntry,
-  type LogError,
-} from "./log-entry.js";
-
-export {
-  DEFAULT_LOGGER_OPTIONS,
-  type LoggerOptions,
-  type LogLevelOption,
-} from "./logger-options.js";
-
-export {
-  ConsoleLogger,
-} from "./console-logger.js";
-
-export {
-  LoggerFactory,
-  type LoggerFactoryOptions,
-  type LoggerImplementation,
-} from "./logger-factory.js";
+export * from "./console/index.js";
