c48246735492dbddfd88106e740ffb2907bb1ac9
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-27 09:17:25 +0100

    refactor(core): add configuration error module

---
+++ packages/core/src/configuration/error/index.ts
@@ -0,0 +1,32 @@
+/**
+ * @lattice/core/configuration/error
+ *
+ * Configuration errors and redaction.
+ */
+
+export {
+  ConfigurationError,
+  ConfigurationSourceError,
+  ConfigurationMissingError,
+  ConfigurationTypeError,
+  ConfigurationSchemaError,
+  ConfigurationConflictError,
+} from "./configurationError.error.js";
+
+export type {
+  ConfigurationErrorIssue,
+} from "./configurationError.error.js";
+
+export {
+  ConfigurationRedactor,
+  createConfigurationRedactor,
+  redactConfiguration,
+  DEFAULT_SENSITIVE_PATTERNS,
+  DEFAULT_REDACTION_VALUE,
+} from "./configurationRedactor.redactor.js";
+
+export type {
+  ConfigurationRedactorOptions,
+  ConfigurationSensitivity,
+  RedactedConfiguration,
+} from "./configurationRedactor.redactor.js";
