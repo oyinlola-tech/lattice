4b285190fbdfdb61a404c82966b7ea7a118e5b61
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-27 09:19:11 +0100

    refactor(core): add context values module

---
+++ packages/core/src/context/values/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @lattice/core/context/values
+ *
+ * Immutable typed context value collection.
+ */
+
+export {
+  ContextValues,
+  createContextValues,
+} from "./contextValues.values.js";
