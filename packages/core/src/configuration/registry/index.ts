daf73ca88c4acd43e692591a7b2b34bd3ab12a41
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-27 09:17:48 +0100

    refactor(core): add configuration registry module

---
+++ packages/core/src/configuration/registry/index.ts
@@ -0,0 +1,25 @@
+/**
+ * @lattice/core/configuration/registry
+ *
+ * Configuration registry and provider.
+ */
+
+export {
+  ConfigurationRegistry,
+  createConfigurationRegistry,
+} from "./configurationRegistry.registry.js";
+
+export type {
+  ConfigurationSection,
+  ConfigurationSectionOptions,
+} from "./configurationRegistry.registry.js";
+
+export {
+  DefaultConfigurationProvider,
+  createConfigurationProvider,
+} from "./configurationProvider.provider.js";
+
+export type {
+  ConfigurationProvider,
+  ConfigurationProviderOptions,
+} from "./configurationProvider.provider.js";
