b189fe5c10e86a51633a052d004c09cf0382c084
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-27 09:17:17 +0100

    refactor(core): add configuration core module

---
+++ packages/core/src/configuration/core/index.ts
@@ -0,0 +1,38 @@
+/**
+ * @lattice/core/configuration/core
+ *
+ * Core configuration types, keys, and source abstractions.
+ */
+
+export {
+  Configuration,
+  createConfiguration,
+} from "./configuration.js";
+
+export type {
+  ConfigurationOptions,
+  ConfigurationValue,
+  ConfigurationSource as ConfigurationValueSource,
+  ConfigurationEntry,
+} from "./configuration.js";
+
+export {
+  createConfigurationKey,
+} from "./configurationKey.key.js";
+
+export type {
+  ConfigurationKey,
+} from "./configurationKey.key.js";
+
+export {
+  BaseConfigurationSource,
+  createConfigurationSource,
+  sortConfigurationSources,
+} from "./configurationSource.source.js";
+
+export type {
+  ConfigurationSource,
+  ConfigurationSourceType,
+  ConfigurationSourceEntry,
+  ConfigurationSourceOptions,
+} from "./configurationSource.source.js";
