c0f814f98a1423b8cee475a66400db0ee2de468e
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-27 09:18:38 +0100

    refactor(core): add context core module

---
+++ packages/core/src/context/core/index.ts
@@ -0,0 +1,33 @@
+/**
+ * @lattice/core/context/core
+ *
+ * Core context types and key management.
+ */
+
+export {
+  Context,
+  createContext,
+  type ContextType,
+  type ExecutionMetadata,
+  type CreateContextOptions,
+} from "./context.js";
+
+export {
+  createExecutionContext,
+  deriveExecutionContext,
+  withExecutionMetadata,
+  getExecutionDuration,
+  type ExecutionContext,
+  type CreateExecutionContextInput,
+} from "./executionContext.context.js";
+
+export {
+  createContextKey,
+  setContextValue,
+  getContextValue,
+  requireContextValue,
+  hasContextValue,
+  deleteContextValue,
+  type ContextKey,
+  type ContextValueStore,
+} from "./contextKey.key.js";
