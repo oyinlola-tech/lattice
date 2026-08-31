d33b30d3567af8a9f8739fe6d1c370f717c29dd5
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-31 07:35:51 +0100

    feat(api): add API constants

---
+++ packages/api/src/api/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * @lattice/api/constants
+ *
+ * Shared constants for the API package.
+ */
+
+/**
+ * Default timeout for API operations (30 seconds).
+ */
+export const DEFAULT_OPERATION_TIMEOUT = 30_000;
+
+/**
+ * Maximum number of interceptors allowed in a pipeline.
+ */
+export const MAX_INTERCEPTORS = 32;
+
+/**
+ * Maximum number of policies allowed on a single operation.
+ */
+export const MAX_POLICIES = 16;
