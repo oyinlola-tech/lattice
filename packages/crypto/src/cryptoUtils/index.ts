3ae32a019209a1d344e0cff76a8a381a13d084ba
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-31 07:12:03 +0100

    refactor(crypto): update utils barrel to export specific utility modules

--- packages/crypto/src/cryptoUtils/index.ts
+++ packages/crypto/src/cryptoUtils/index.ts
@@ -1,7 +1,14 @@
 /**
  * @lattice/crypto/cryptoUtils
  *
- * Cryptographic utility functions and helpers.
+ * Utility functions for encoding, decoding, and validating
+ * binary data and text representations.
  */
 
-export * from './cryptoUtils.helper.js';
+export * from "./cryptoUtils.compare.js";
+export * from "./cryptoUtils.conversion.js";
+export * from "./cryptoUtils.number.js";
+export * from "./cryptoUtils.text.js";
+export * from "./cryptoUtils.validation.js";
+
+export * from "./encoding/index.js";
