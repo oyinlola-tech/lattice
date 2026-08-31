2bd83e45262efb94cb8376c20d457382ba478730
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-31 07:04:16 +0100

    refactor(crypto): re-export crypto errors from @lattice/errors

--- packages/crypto/src/cryptoErrors/index.ts
+++ packages/crypto/src/cryptoErrors/index.ts
@@ -2,6 +2,23 @@
  * @lattice/crypto/cryptoErrors
  *
  * Cryptographic operation error types.
+ *
+ * Re-exports from @lattice/errors to maintain a single
+ * error hierarchy across the framework.
  */
 
-export * from './cryptoError.base.js';
+export {
+  CryptoError,
+  CryptoOperation,
+  createCryptoError,
+  isCryptoError,
+  cryptoHashError,
+  cryptoCipherError,
+  cryptoSignatureError,
+  cryptoKeyDerivationError,
+  cryptoKeyError,
+} from "@lattice/errors";
+
+export type {
+  CryptoErrorOptions,
+} from "@lattice/errors";
