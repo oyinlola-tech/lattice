bfa73e05208003c5c18b9a83bd0259fac54ca474
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-31 07:10:30 +0100

    refactor(crypto): add operation exports (cipher, hash, password, token) to service barrel

--- packages/crypto/src/cryptoService/index.ts
+++ packages/crypto/src/cryptoService/index.ts
@@ -5,3 +5,25 @@
  */
 
 export * from './cryptoService.core.js';
+
+export {
+  serviceEncrypt,
+  serviceDecrypt,
+} from './operations/cryptoService.cipher.js';
+
+export {
+  serviceHash,
+  serviceHashHex,
+} from './operations/cryptoService.hash.js';
+
+export {
+  serviceHashPassword,
+  serviceVerifyPassword,
+} from './operations/cryptoService.password.js';
+
+export {
+  serviceGenerateToken,
+  serviceGenerateOtp,
+  serviceHashToken,
+  serviceVerifyToken,
+} from './operations/cryptoService.token.js';
