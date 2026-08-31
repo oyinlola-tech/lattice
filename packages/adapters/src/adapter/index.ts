ab2dd78f82f9e4c8544f6ea7b92044213255a891
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-31 07:31:28 +0100

    feat(adapters): add adapter barrel export

---
+++ packages/adapters/src/adapter/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @lattice/adapters/adapter
+ *
+ * Core adapter types and registry.
+ */
+
+export type { Adapter } from "./adapter.type.js";
+export { AdapterRegistry } from "./adapter.registry.js";
